d17472dc2a32dca4f78fcdfc111bd0c23b90d6cf
Julien Biezemans <jb@jbpros.com>
2013-10-11 16:06:22 +0200

    ng-repeat
    The built-in ng-repeat directive allows to iterate over a collection and repeat presentation structures.

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -2,34 +2,16 @@
 
   var app = angular.module("app", []);
 
-  app.controller("AController", function ($scope, greet, greeting) {
-    $scope.data = greeting;
-    $scope.greet = greet;
-  });
-
-  app.controller("BController", function ($scope, greet, greeting) {
-    $scope.data = greeting;
-    $scope.greet = greet;
-  });
-
-  app.factory("defaultGreeting", function () {
-    return "Bonjour à vous!";
-  });
-
-  app.factory("greeting", function (defaultGreeting) {
-    return {};
-  });
-
-  app.factory("greet", function (defaultGreeting) {
-    return function (greeting) {
-      alert(greeting || defaultGreeting);
-    }
-  });
-
-  app.filter("quote", function () {
-    return function (text) {
-      return '"' + (text || '') + '"';
-    };
+  app.controller("AnimalsController", function ($scope) {
+    $scope.animals = [
+      { name: "capybara" },
+      { name: "bear" },
+      { name: "snake" },
+      { name: "spider" },
+      { name: "jellyfish" },
+      { name: "rat" },
+      { name: "whale" }
+    ];
   });
 
 })(angular);
