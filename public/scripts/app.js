c73c16baf554876147ea9d89050f23d54b15920a
Julien Biezemans <jb@jbpros.com>
2013-10-11 15:09:59 +0200

    Services: a very simple service
    Services are used to encapsulate all kinds of logic and data that are
    potentially reusable.

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -2,9 +2,15 @@
 
   var app = angular.module("app", []);
 
-  app.controller("AppController", function ($scope) {
+  app.controller("AppController", function ($scope, defaultGreeting) {
     $scope.data = { message: "Hi there!" };
-    $scope.greet = function () { alert($scope.data.message); };
+    $scope.greet = function () {
+      alert($scope.data.message || defaultGreeting);
+    };
+  });
+
+  app.factory("defaultGreeting", function () {
+    return "Bonjour à vous!";
   });
 
 })(angular);
