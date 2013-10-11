cfb6262b7eb35b32793d0cce9984a797cdd2d313
Julien Biezemans <jb@jbpros.com>
2013-10-11 15:06:23 +0200

    Modules: namespace things
    Instead of exposing our controllers and other future components on the
    global `window` object, we can define them inside a module. Angular
    modules let you create namespaces that don't pollute the global scope.

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,4 +1,10 @@
-function AppController($scope) {
-  $scope.data = { message: "Hi there!" };
-  $scope.greet = function () { alert($scope.data.message); };
-}
+(function (angular) {
+
+  var app = angular.module("app", []);
+
+  app.controller("AppController", function ($scope) {
+    $scope.data = { message: "Hi there!" };
+    $scope.greet = function () { alert($scope.data.message); };
+  });
+
+})(angular);
