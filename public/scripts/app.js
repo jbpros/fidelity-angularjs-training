be01a1365a92ca643e9fb741d65def360a54ca07
Julien Biezemans <jb@jbpros.com>
2013-10-12 17:39:51 +0200

    Directive isolated scope and & bindings
    & bindings in directive allow for wrapping an expression executed within the parent scope.

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -2,11 +2,19 @@
 
   var app = angular.module("app", []);
 
+  app.controller("MammalController", function ($scope) {
+    $scope.makeSomeNoise = function () {
+      alert("Roaarrrr!");
+    };
+  });
+
   app.directive("mammal", function() {
     return {
       restrict: "E",
-      scope: {},
-      template: "<div class='well'><input ng-model='mammal.name'><br>Hi, my name is {{ mammal.name }}!</div>",
+      scope: {
+        scream: "&"
+      },
+      template: "<div class='well'><input ng-model='mammal.name'><br>Hi, my name is {{ mammal.name }}!<button ng-click='scream()'>Scream!</button></div>",
       replace: true
     };
   });
