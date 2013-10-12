1de0ffe50d9d1706bc0fe4969aef51503853c60a
Julien Biezemans <jb@jbpros.com>
2013-10-12 18:01:02 +0200

    Directive: = scope attribute
    = lets you bind data from the parent scope to the directive scope. This is two-way binding, meaning changes inside the directive are propagated to the outside, and vice versa.

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -3,15 +3,17 @@
   var app = angular.module("app", []);
 
   app.controller("MammalController", function ($scope) {
+    $scope.name = 'Lassie'
   });
 
   app.directive("mammal", function() {
     return {
       restrict: "E",
       scope: {
-        name: "@"
+        name: "="
       },
-      template: "<div>I'm a mammal and my name is {{ name }}</div>"
+      template: "<div>I'm a mammal and my name is {{ name }}</div>" +
+        "<div><input ng-model='name'></div>"
     };
   });
 
