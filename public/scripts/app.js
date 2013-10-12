b2e079f22fb30e0b8345bed9190c8a36a6156dd4
Julien Biezemans <jb@jbpros.com>
2013-10-12 17:54:27 +0200

    Directive: refactor and use @
    @ scope members are used to pass a simple string attribute to the directive

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -8,10 +8,10 @@
   app.directive("mammal", function() {
     return {
       restrict: "E",
-      template: "<div>I'm a mammal and my name is {{ name }}</div>",
-      link: function (scope, element, attributes) {
-        scope.name = attributes.name;
-      }
+      scope: {
+        name: "@"
+      },
+      template: "<div>I'm a mammal and my name is {{ name }}</div>"
     };
   });
 
