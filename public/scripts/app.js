40742e6f3f4b77a26e02b6bc3b171a4e1eea0570
Julien Biezemans <jb@jbpros.com>
2013-10-12 17:52:27 +0200

    Directive: naive attribute string

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -3,19 +3,15 @@
   var app = angular.module("app", []);
 
   app.controller("MammalController", function ($scope) {
-    $scope.makeSomeNoise = function () {
-      alert("Roaarrrr!");
-    };
   });
 
   app.directive("mammal", function() {
     return {
       restrict: "E",
-      scope: {
-        scream: "&"
-      },
-      template: "<div class='well'><input ng-model='mammal.name'><br>Hi, my name is {{ mammal.name }}!<button ng-click='scream()'>Scream!</button></div>",
-      replace: true
+      template: "<div>I'm a mammal and my name is {{ name }}</div>",
+      link: function (scope, element, attributes) {
+        scope.name = attributes.name;
+      }
     };
   });
 
