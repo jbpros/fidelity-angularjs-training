8d8f200a10e29a917ecf324e6b3df4616a302877
Julien Biezemans <jb@jbpros.com>
2013-10-15 22:56:21 +0100

    scope.$watch: listen to a simple scope change
    scope.$watch is used to observe changes on the scope and trigger callbacks when such changes occur

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -14,7 +14,12 @@
       },
       transclude: true,
       template: "<div>I'm a mammal and my name is {{ name }}.</div><div ng-transclude></div>" +
-        "<div><input ng-model='name'></div>"
+        "<div><input ng-model='name'></div>",
+      link: function (scope, element, attributes) {
+        scope.$watch('name', function (newName) {
+          console.log("name changed to:", newName);
+        });
+      }
     };
   });
 
